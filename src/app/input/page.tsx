16362c75bfa5e7415e7255be0e2f8a13176298d3
Soham <realsoham774@gmail.com>
2024-02-17 18:04:27 +0530

    added shadcn

---
+++ src/app/input/page.tsx
@@ -0,0 +1,11 @@
+import { InputForm } from '@/components/component/input-form'
+import React from 'react'
+
+export default function Input() {
+  return (
+    <div>Input
+        <InputForm/>
+    </div>
+  ) 
+}
+
